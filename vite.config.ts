9a607e4aede4f039bc03d4b54bb566f1943eb466
saili80354586 <saili80354586@gmail.com>
2026-03-22 21:36:38 +0800

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8201',
+        changeOrigin: true,
+      },
+    },
+  },
+  preview: {
+    port: 4173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8201',
+        changeOrigin: true,
+      },
+    },
+  },
+})
